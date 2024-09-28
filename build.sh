861228ba1f4142807503cd996f7888f37acb72f1
unknown <nick>
2024-09-27 21:53:34 -0300

    new bash

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
+
+python ./point_system/manage.py collectstatic --noinput
+python ./point_system/manage.py makemigrations
+python ./point_system/manage.py migrate
